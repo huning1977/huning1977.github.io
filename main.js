9556d3bcdd3fdf2b3f864f7a377d4cf24816aacb
huning1977 <153514974+huning1977@users.noreply.github.com>
2024-08-10 17:32:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "C:/Users/hunin/Desktop/webprojects/test-site/images/firefox.png") {
-    myImage.setAttribute("src", "C:/Users/hunin/Desktop/webprojects/test-site/images/chrome.png");
+  if (mySrc === "/images/firefox.png") {
+    myImage.setAttribute("src", "/images/chrome.png");
   } else {
-    myImage.setAttribute("src", "C:/Users/hunin/Desktop/webprojects/test-site/images/firefox.png");
+    myImage.setAttribute("src", "/images/firefox.png");
   }
 };
 let myButton = document.querySelector("button");
